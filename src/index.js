cc615dc8f88fc9362f7f318aa33bcea49f8bb449
Tatsiana Hladkaya <tatyana.hladkaya@gmail.com>
2024-08-29 20:57:56 +0300

    add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function toReadable (number) {
-  
-}
+
+  const units = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+  const teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+  const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+  if(number === 0) return "zero";
+
+  let result = "";
+
+  if (Math.floor(number / 100) > 0) {
+    result += units[Math.floor(number / 100)] + " hundred";
+    number %= 100;
+    if (number > 0) {
+      result += " ";
+    };
+  }
+
+  if (number >= 10 && number < 20) {
+      result += teens[number - 10];
+  } else if (number >= 20) {
+      result += tens[Math.floor(number / 10)];
+      number %= 10;
+      if (number > 0) {
+        result += " ";
+      };
+  }
+
+  if (number > 0 && number < 10) {
+      result += units[number];
+  }
+
+return result.trim();
+
+};
